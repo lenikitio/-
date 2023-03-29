3bf57f6857c7796b17468547daed97cce657ed22
nikita <tolstoguzov.nikita98@mail.ru>
2023-03-29 03:19:06 +0300

    решение

--- Program.cs
+++ Program.cs
@@ -4,3 +4,51 @@
 
 // ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 // ["Russia", "Denmark", "Kazan"] -> []
+
+Console.Clear();
+
+string[] GetArray(int size)
+{
+    string[] result = new string[size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+        result[i] = Console.ReadLine();
+    }
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i == 0) Console.Write("[" + array[i] + ", ");
+        else if(i == array.Length - 1) Console.Write(array[i] + "]");
+        else Console.Write(array[i] + ", ");
+    }
+}
+
+string[] GetArrayThreeElem(string[] array)
+{
+    int count = 0;
+    foreach(string elem in array)
+        if(elem.Length <= 3) count++;
+    string[] ThreeArray = new string[count];
+    int i = 0;
+    foreach(string elem in array)
+    {
+        if(elem.Length <= 3) 
+        {
+            ThreeArray[i] = elem;
+            i++;
+        }
+        if(i == count) break;
+    }
+    return ThreeArray;
+}
+
+string[] OurArray = GetArray(4);
+PrintArray(OurArray);
+Console.Write(" -> ");
+string[] ChangeArray = GetArrayThreeElem(OurArray);
+PrintArray(ChangeArray);
